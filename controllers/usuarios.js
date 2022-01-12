eb51b8db1dabfb76c7363552f0ea028e15ba4067
leoor22 <leonelrivas.rl@gmail.com>
2022-01-12 16:50:33 -0300

    Usuarios CRUD listo - 12-01-2022

---
+++ controllers/usuarios.js
@@ -0,0 +1,79 @@
+const { response, request, query } = require("express");
+const bcryptjs = require("bcryptjs");
+
+const Usuario = require("../models/usuario");
+
+const usuariosGet = async (req, res = response) => {
+  const { limite = 5, desde = 0 } = req.query;
+  const query = { estado: true };
+
+  const [total, usuarios] = await Promise.all([
+    Usuario.countDocuments(query),
+    Usuario.find(query).skip(Number(desde)).limit(Number(limite)),
+  ]);
+
+  res.json({
+    total,
+    usuarios,
+  });
+};
+
+const usuariosPost = async (req, res) => {
+  const { nombre, correo, password, rol } = req.body;
+  const usuario = new Usuario({ nombre, correo, password, rol });
+
+  // Verificar si el correo existe
+
+  // Encriptar la contraseña
+  const salt = bcryptjs.genSaltSync();
+  usuario.password = bcryptjs.hashSync(password, salt);
+
+  // Guardar en DB
+  await usuario.save();
+
+  res.json({
+    usuario,
+  });
+};
+
+const usuariosPut = async (req, res) => {
+  const { id } = req.params;
+  const { _id, password, google, correo, ...resto } = req.body;
+
+  // TODO validar contra base de datos
+
+  if (password) {
+    // Encriptar la contraseña
+    const salt = bcryptjs.genSaltSync();
+    resto.password = bcryptjs.hashSync(password, salt);
+  }
+
+  const usuario = await Usuario.findByIdAndUpdate(id, resto);
+
+  res.json(usuario);
+};
+
+const usuariosDelete = async (req, res) => {
+  const { id } = req.params;
+
+  // Fisicamente lo borramos
+  // const usuario = await Usuario.findByIdAndDelete(id);
+
+  const usuario = await Usuario.findByIdAndUpdate(id, { estado: false });
+
+  res.json(usuario);
+};
+
+const usuariosPatch = (req, res) => {
+  res.json({
+    msg: "patch API - controlador",
+  });
+};
+
+module.exports = {
+  usuariosGet,
+  usuariosPost,
+  usuariosPut,
+  usuariosDelete,
+  usuariosPatch,
+};
--- models/server.js
+++ models/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 var cors = require("cors");
+const { dbConnection } = require("../DB/config");
 
 class Server {
   constructor() {
@@ -7,11 +8,18 @@ class Server {
     this.port = process.env.PORT;
     this.usuariosPath = "/api/usuarios";
 
+    // Connect to DB
+    this.conectarDB();
+
     // Middlewares
     this.middlewares();
 
     // Rutas de mi aplicacion
     this.routes();
+  }
+
+  async conectarDB() {
+    await dbConnection();
   }
 
   middlewares() {
